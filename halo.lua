c9a2a3734aa8fa3b95edec52f4775a517f3c658f
Arkiver2 <Arkiver@hotmail.com>
2014-11-01 12:06:46 +0100

    halo.lua: fix: turn gsub around

--- halo.lua
+++ halo.lua
@@ -101,7 +101,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
                 table.insert(urls, { url=customurl })
               end
               if string.match(customurl, "/Stats/GameStatsHalo3%.aspx%?gameguid=[0-9]+") then
-                newcustomurl = string.gsub(customurl, "GameFiles%.aspx%?guid=", "GameStatsHalo3%.aspx%?gameguid=")
+                newcustomurl = string.gsub(customurl, "GameStatsHalo3%.aspx%?gameguid=", "GameFiles%.aspx%?guid=")
                 if downloaded[newcustomurl] ~= true and addedtolist[newcustomurl] ~= true then
                   table.insert(urls, { url=newcustomurl })
                 end
@@ -131,7 +131,7 @@ wget.callbacks.get_urls = function(file, url, is_css, iri)
                 table.insert(urls, { url=customurl })
               end
               if string.match(customurl, "/Stats/GameStatsHalo3%.aspx%?gameguid=[0-9]+") then
-                newcustomurl = string.gsub(customurl, "GameFiles%.aspx%?guid=", "GameStatsHalo3%.aspx%?gameguid=")
+                newcustomurl = string.gsub(customurl, "GameStatsHalo3%.aspx%?gameguid=", "GameFiles%.aspx%?guid=")
                 if downloaded[newcustomurl] ~= true and addedtolist[newcustomurl] ~= true then
                   table.insert(urls, { url=newcustomurl })
                 end
